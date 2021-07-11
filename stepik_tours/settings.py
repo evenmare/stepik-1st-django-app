571dc59bd63c5fcad581cce07312fc5843f30145
evenmare <kir9ver@gmail.com>
2021-07-11 18:25:05 +0300

    don't wanna you steel my key

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ugm8wqnkbj14uv611ih&4kaqfe-ww(p3o@d%54(i1=4uco5e0k'
+SECRET_KEY = 'paste your key here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
