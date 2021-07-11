90245ccd4eeab4435bc93cf9f7618d8402e769c0
evenmare <kir9ver@gmail.com>
2021-07-11 18:37:02 +0300

    updated to post on Heroku

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'paste your key here'
+SECRET_KEY = 'dw(4ma*314yu1tx5x)d6#3k5pot#s#m*q=j#pdt^c*^^el#@sj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
